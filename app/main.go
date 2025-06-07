2ae80e4c6f3e0a0a6af8059577221bfb791dce1c
avdes96 <avdes96@gmail.com>
2025-06-07 11:21:22 +0100

    Shell outputs not found message for invalid commands

--- app/main.go
+++ app/main.go
@@ -11,5 +11,10 @@ func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error whilst reading user input: %s", err)
+		os.Exit(1)
+	}
+	fmt.Println(command[:len(command)-1] + ": command not found")
 }
