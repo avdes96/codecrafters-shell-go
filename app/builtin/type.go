e21d021a6b10b80fda4b7e30718f3c1f84376d90
avdes96 <avdes96@gmail.com>
2025-06-08 09:10:35 +0100

    Added search for executables files in type builtin

--- app/builtin/type.go
+++ app/builtin/type.go
@@ -1,16 +1,49 @@
 package builtin
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 
 type Type struct {
 	Builtins *map[string]Builtin
 }
 
 func (t Type) Run(args []string) {
-	builtins := *t.Builtins
-	if _, ok := builtins[args[0]]; ok {
-		fmt.Println(args[0] + " is a shell builtin")
-		return
+	command := args[0]
+	if t.isBuiltIn(command) {
+		fmt.Println(command + " is a shell builtin")
+	} else if ok, exec := t.isExecutable(command); ok {
+		fmt.Println(command + " is " + exec)
+	} else {
+		fmt.Println(command + ": not found")
 	}
-	fmt.Println(args[0] + ": not found")
+}
+
+func (t Type) isBuiltIn(command string) bool {
+	builtins := *t.Builtins
+	if _, ok := builtins[command]; ok {
+		return true
+	}
+	return false
+}
+
+func (t Type) isExecutable (command string) (bool, string) {
+	path := os.Getenv("PATH")
+	dirs := strings.Split(path, string(os.PathListSeparator))
+	for _, dir := range dirs {
+		files, err := os.ReadDir(dir)
+		if err != nil {
+			continue
+		}
+		for _, file := range files {
+			if file.Name() == command {
+				return true, dir + string(os.PathSeparator) + file.Name()
+			}
+		}
+	}
+
+	return false, ""
 }
