a73ea4e26c55ddeaeb99c5a8e2e95d5b9332cb8b
avdes96 <avdes96@gmail.com>
2025-06-09 22:42:13 +0100

    Added support for double quotes in args

--- app/utils/utils.go
+++ app/utils/utils.go
@@ -13,15 +13,37 @@ func FindExecutablePath(executable string) string {
 	return path
 }
 
+const (
+	NOT_IN_QUOTE = iota
+	IN_SINGLE_QUOTE
+	IN_DOUBLE_QUOTE
+)
+
 
 func ParseArgs(s string) []string {
 	output := []string{}
-	inSingleQuote := false
+	status := NOT_IN_QUOTE
 	buffer := ""
 	for _, c := range s {
 		if c == '\'' {
-			inSingleQuote = !inSingleQuote
-		} else if unicode.IsSpace(c) && !inSingleQuote {
+			switch status {
+			case NOT_IN_QUOTE:
+				status = IN_SINGLE_QUOTE
+			case IN_SINGLE_QUOTE:
+				status = NOT_IN_QUOTE
+			case IN_DOUBLE_QUOTE:
+				buffer += string(c)
+			}
+		} else if c == '"' {
+			switch status {
+			case NOT_IN_QUOTE:
+				status = IN_DOUBLE_QUOTE
+			case IN_DOUBLE_QUOTE:
+				status = NOT_IN_QUOTE
+			case IN_SINGLE_QUOTE:
+				buffer += string(c)
+			}
+		} else if unicode.IsSpace(c) && status == NOT_IN_QUOTE {
 			if buffer != "" {
 				output = append(output, buffer)
 				buffer = ""
