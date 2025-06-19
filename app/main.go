9a9474b306da08adcfffbca6a269d59229637c7c
avdes96 <avdes96@gmail.com>
2025-06-20 00:19:47 +0100

    Refactor: Create a separate shell package with a shell struct and run function

--- app/main.go
+++ app/main.go
@@ -1,68 +1,12 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-
-	"github.com/codecrafters-io/shell-starter-go/app/builtin"
-	"github.com/codecrafters-io/shell-starter-go/app/executable"
-	"github.com/codecrafters-io/shell-starter-go/app/linereader"
 	"github.com/codecrafters-io/shell-starter-go/app/logger"
-	"github.com/codecrafters-io/shell-starter-go/app/utils"
+	"github.com/codecrafters-io/shell-starter-go/app/shell"
 )
 
 func main() {
 	logger.InitLogger()
-
-	builtins := map[string]builtin.Builtin{}
-	builtins["exit"] = builtin.Exit{}
-	builtins["echo"] = builtin.Echo{}
-	builtins["type"] = builtin.Type{Builtins: &builtins}
-	builtins["pwd"] = builtin.Pwd{}
-	builtins["cd"] = builtin.Cd{}
-	
-	var config utils.ShellConfig
-
-	isNewLine, initialState := true, ""
-	reader := linereader.New(bufio.NewReader(os.Stdin))
-	for {
-		if isNewLine {
-			fmt.Fprint(os.Stdout, "$ ")
-			isNewLine = false
-		}
-
-		invoker, userInput, err := reader.ReadLine(initialState)
-		switch invoker {
-		case "\n":
-			fmt.Fprint(os.Stdout, "\n")
-			if err != nil {
-				log.Fatalf("Error whilst reading user input: %v", err)
-			}
-			userInputSplit := utils.ParseString(userInput)
-			command := userInputSplit[0]
-			args := []string{}
-			if len(userInputSplit) > 1 {
-				args = userInputSplit[1:]
-			}
-			args, config = utils.ParseArgs(args)
-			if b, ok := builtins[command]; ok {
-				b.Run(args, config)
-			} else if path := utils.FindExecutablePath(command); path != "" {
-				executable.RunExecutable(command, config, args)
-			} else {
-				fmt.Fprint(config.StdErrFile, command + ": command not found\n")
-			}
-			isNewLine = true
-		case "\t":
-			var output string
-			if userInput == "ech" {
-				output = "o"
-			} else {
-				output = "t"
-			}
-			fmt.Fprintf(os.Stdout, "%s ", output)
-		}		
-	}
+	shell := shell.New()
+	shell.Run()
 }
---
+++ app/shell/shell.go
@@ -0,0 +1,83 @@
+package shell
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/codecrafters-io/shell-starter-go/app/builtin"
+	"github.com/codecrafters-io/shell-starter-go/app/executable"
+	"github.com/codecrafters-io/shell-starter-go/app/linereader"
+	"github.com/codecrafters-io/shell-starter-go/app/utils"
+)
+
+type shell struct {
+	builtins map[string]builtin.Builtin
+}
+
+func New() shell {
+	return shell{
+		builtins: getBuiltins(),
+	}
+}
+
+func getBuiltins() map[string]builtin.Builtin {
+	builtins := map[string]builtin.Builtin{}
+	builtins["exit"] = builtin.Exit{}
+	builtins["echo"] = builtin.Echo{}
+	builtins["type"] = builtin.Type{Builtins: &builtins}
+	builtins["pwd"] = builtin.Pwd{}
+	builtins["cd"] = builtin.Cd{}
+	return builtins
+}
+
+func (s shell) Run() {
+	isNewLine, userInput := true, ""
+	reader := linereader.New(bufio.NewReader(os.Stdin))
+	for {
+		if isNewLine {
+			fmt.Fprint(os.Stdout, "$ ")
+			isNewLine = false
+		}
+		invoker, userInput, err := reader.ReadLine(userInput)
+		if err != nil {
+			log.Fatalf("Error whilst reading user input: %v", err)
+		}
+		switch invoker {
+		case "\n":
+			s.executeCommand(userInput)
+			isNewLine, userInput = true, ""
+		case "\t":
+			endOfWord := s.autocomplete(userInput)
+			fmt.Fprint(os.Stdout, endOfWord)
+			userInput += endOfWord
+		}		
+	}
+}
+
+func (s shell) executeCommand(userInput string) {
+	var config utils.ShellConfig
+	fmt.Fprint(os.Stdout, "\n")
+	userInputSplit := utils.ParseString(userInput)
+	command := userInputSplit[0]
+	args := []string{}
+	if len(userInputSplit) > 1 {
+		args = userInputSplit[1:]
+	}
+	args, config = utils.ParseArgs(args)
+	if b, ok := s.builtins[command]; ok {
+		b.Run(args, config)
+	} else if path := utils.FindExecutablePath(command); path != "" {
+		executable.RunExecutable(command, config, args)
+	} else {
+		fmt.Fprint(config.StdErrFile, command + ": command not found\n")
+	}
+}
+
+func (s shell) autocomplete(userInput string) string {
+	if userInput == "ech" {
+		return "o "
+	}
+	return "t "
+}
