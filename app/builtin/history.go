4d141d398bf66f4e0179ffc108b5d713b2aa7ef9
avdes96 <avdes96@gmail.com>
2025-06-24 01:00:25 +0100

    History command has support for limiting the number of historical commands displayed

--- app/builtin/history.go
+++ app/builtin/history.go
@@ -2,6 +2,8 @@ package builtin
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 
 	"github.com/codecrafters-io/shell-starter-go/app/utils"
 )
@@ -9,11 +11,37 @@ import (
 type History struct{HistoryList *[]string}
 
 func (h *History) Run(cmd *utils.ShellCommand) {
-	for i, prevCommand := range *h.HistoryList {
-		fmt.Fprintf(cmd.StdOutFile, "\t%d %s\n", i+1, prevCommand)
+	if len(cmd.Args) > 1 {
+		fmt.Fprint(cmd.StdOutFile, "Usage: history <n>\n")
+		return
 	}
+	if len(cmd.Args) == 0 {
+		n := len(*h.HistoryList)
+		h.printHistory(cmd.StdOutFile, n)
+		return
+	}
+
+	nStr := cmd.Args[0]
+	n, err := strconv.Atoi(nStr)
+	if err != nil {
+		fmt.Fprintf(cmd.StdOutFile, "history: %s: numeric argument required\n", nStr)
+		return
+	}
+	if n < 0 {
+		fmt.Fprintf(cmd.StdOutFile, "history: %d: numeric argument must be non-negative\n", n)
+		return
+	}
+	h.printHistory(cmd.StdOutFile, n)
+	return
 }
 
 func NewHistory(h *[]string) *History {
 	return &History{HistoryList: h}
+}
+
+func (h *History) printHistory(out *os.File, n int) {
+	start := max(0, len(*h.HistoryList) - n)
+	for i, prevCommand := range (*h.HistoryList)[start:] {
+		fmt.Fprintf(out, "\t%d %s\n", i+1, prevCommand)
+	}
 }
